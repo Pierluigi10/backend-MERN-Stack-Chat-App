1a8aff96488a549b2ce3965ba4310c48a387e113
pierluigi <pierluigibaiano@gmail.com>
2021-12-15 15:43:57 +0100

    create env with port number. install dotenv

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { chats } from "./data/data.js";
+import dotenv from "dotenv";
+
+dotenv.config()
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Api is running");
