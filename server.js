193b531fc5689001206bc92f60139b9db65b0f55
pierluigi <pierluigibaiano@gmail.com>
2021-12-15 15:33:51 +0100

    import chats to sever.js and create a new endpoint (api/chat)

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
-import express from 'express';
+import express from "express";
+import { chats } from "./data/data.js";
 
-const app=express();
+const app = express();
 const PORT = 5000;
 
-
 app.get("/", (req, res) => {
-    res.send("Api is running")
-})
+  res.send("Api is running");
+});
+
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
 app.listen(PORT, (req, res) => {
-    console.log(`API listening on port http://localhost:${PORT}`);
-  });
+  console.log(`API listening on port http://localhost:${PORT}`);
+});
