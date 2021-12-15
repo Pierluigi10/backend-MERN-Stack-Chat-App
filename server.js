6c0bcdeb95931d0f95374d2c54bd6e9f3790d881
pierluigi <pierluigibaiano@gmail.com>
2021-12-15 15:29:18 +0100

    app.get works!

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const app=express();
 const PORT = 5000;
 
 
-
+app.get("/", (req, res) => {
+    res.send("Api is running")
+})
 
 app.listen(PORT, (req, res) => {
     console.log(`API listening on port http://localhost:${PORT}`);
