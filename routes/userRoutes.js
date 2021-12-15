8c9b41c7616c202e001956c59c5c056c6458500a
pierluigi <pierluigibaiano@gmail.com>
2021-12-15 16:16:06 +0100

    create userControllers and test in userRoutes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,10 @@
 import express from "express";
 
+import { registerUser } from "../controllers/userControllers.js";
+
 const router = express.Router();
 
-// router.route("/").post(registerUser);
+router.route("/").post(registerUser);
 // router.rpost("/login", authUser);
 
 export default router;
